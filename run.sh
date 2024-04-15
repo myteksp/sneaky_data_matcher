c3d280a15efd2164e679ae9df6da7e1e686946b7
Dmitry Paradny <dmitry.paradny@gmail.com>
2024-04-15 23:08:03 +0300

    first commit

--- run.sh
+++ run.sh
@@ -8,5 +8,6 @@ export NEO4J_DB="neo4j"
 export NEO4J_URI="neo4j://62.169.24.194"
 export NEO4J_USER="neo4j"
 export NEO4J_PASSWORD="Pjws94319431"
+export PORT="80"
 
 java -jar build/libs/server-0.0.1-SNAPSHOT.jar
