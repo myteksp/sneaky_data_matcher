9f5ddc9c54641242010f3be1e3879460c80b94ec
Dmitry Paradny <dmitry.paradny@gmail.com>
2024-06-11 17:20:51 +0300

    mysql integration

--- run.sh
+++ run.sh
@@ -6,5 +6,8 @@ export NEO4J_URI="neo4j://62.169.24.194"
 export NEO4J_USER="neo4j"
 export NEO4J_PASSWORD="Pjws94319431"
 export PORT="80"
+export MARIA_URL="jdbc:mariadb://185.222.241.151:3306/"
+export MARIA_PASSWORD="Pjws94319431"
+
 
 java -jar build/libs/server-0.0.1-SNAPSHOT.jar
