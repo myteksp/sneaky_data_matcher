608884aded546d7afecf704fb4f4de128f83e69b
Dmitry Paradny <dmitry.paradny@gmail.com>
2024-06-25 20:59:58 +0300

    mysql integration

--- src/main/java/com/dataprocessor/server/utils/StringTransformer.java
+++ src/main/java/com/dataprocessor/server/utils/StringTransformer.java
@@ -21,11 +21,22 @@ public class StringTransformer {
         return result;
     }
 
+    private static final String trimQuotes(final String string){
+        String res = string.trim();
+        while (res.startsWith("'") || res.startsWith("\"")){
+            res = res.substring(1).trim();
+        }
+        while (res.endsWith("'") || res.endsWith("\"")){
+            res = res.substring(0, res.length()-1).trim();
+        }
+        return res;
+    }
+
     private static final String normalize(final String str){
         final StringTokenizer tokenizer = new StringTokenizer(str);
         final StringBuilder sb = new StringBuilder(str.length());
         while (tokenizer.hasMoreTokens()) {
-            final String token = tokenizer.nextToken().trim();
+            final String token = trimQuotes(tokenizer.nextToken().trim());
             if (!token.isBlank()){
                 sb.append(token).append(' ');
             }
